4af534a257439105f1e252c37eed848fe30c4029
bellothornus <damia_iv@yahoo.es>
2020-10-28 19:40:46 +0100

    test(Duplicate_Encoder): :art: mejorar estructura casos tests
    Hemos mejorado la estructura de los casos tests para que den casos distintos unos resultados esperados
    
    Ningún problema ne realidad, ahora los casos tests son mas precisos y fáciles de leer

--- test/test_general_test.py
+++ test/test_general_test.py
@@ -1,7 +1,10 @@
 from src.code import duplicate_encode
 
-def test_din():
+def test_normal():
     assert duplicate_encode("din") == "((("
+def test_duplicate_and_not_duplicate():
     assert duplicate_encode("recede") == "()()()"
+def test_lower_upper_text():
     assert duplicate_encode("Success") == ")())())", "should ignore upper and lower cases"
+def test_special_character():
     assert duplicate_encode("(( @") == "))((", "should work too with special characters"
