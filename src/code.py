1b5959829a1a77e80313448d615a65deb42ed737
bellothornus <damia_iv@yahoo.es>
2020-10-28 19:17:23 +0100

    feat(Duplicate_Encoder): Docs,Code and testing
    Se ha implantado la documentacion, el código y los casos tests con pytest para poder realiar el kata
    
    No ha habido ningún problema

---
+++ src/code.py
@@ -0,0 +1,15 @@
+def duplicate_encode(string):
+    encode = ""
+    characters = list(string)
+    for character in characters:
+        if characters.count(character) > 1:
+            encode += "("
+        else:
+            encode += ")"
+    return encode
+
+if __name__ == "__main__":
+    assert duplicate_encode("din") == "((("
+    assert duplicate_encode("recede") == "()()()"
+    assert duplicate_encode("Success") == ")())())", "should ignore upper and lower cases"
+    assert duplicate_encode("(( @") == "))((", "should work too with special characters"
---
+++ test/test_general_test.py
@@ -0,0 +1,7 @@
+from src.code.py import duplicate_encode
+
+def test_din():
+    assert duplicate_encode("din") == "((("
+    assert duplicate_encode("recede") == "()()()"
+    assert duplicate_encode("Success") == ")())())", "should ignore upper and lower cases"
+    assert duplicate_encode("(( @") == "))((", "should work too with special characters"
